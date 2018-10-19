e083cb2c19835cb5b6383fcc414546da4087cec1
Tulun <jaykiraly@gmail.com>
2018-10-19 15:32:48 -0700

    returns, not return

--- remix-proj/inbox.sol
+++ remix-proj/inbox.sol
@@ -3,15 +3,17 @@ pragma solidity ^0.4.17;
 contract Inbox {
     string public message;
     
+    // Constructor function. Has the same name as the contract.
     function Inbox(string initialMessage) public {
         message = initialMessage;
     }
     
+    // Public functions are callable from anyone on the blockchain
     function setMessage(string newMessage) public {
         message = newMessage;
     }
     
-    function getMessage() public view return (string) {
+    function getMessage() public view returns (string) {
         return message;
     }
 }
