efd408827e28589d822f9ed7a2567f9bb8f21f56
Jason Kiraly <jaykiraly@gmail.com>
2018-10-23 14:16:12 -0700

    got deployed contract bqby.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,19 +1,23 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const { interface, bytecode } = require('../compile');
 
 // Provider might change depending what network we point to on the blockchain.
 const web3 = new Web3(ganache.provider());
 
+let accounts, inbox;
  beforeEach(async () => {
   // Get a list of all accounts
-  const accounts = await web3.eth.getAccounts();
-  console.log(accounts);
+  accounts = await web3.eth.getAccounts();
   // Use one of the accounts to deploy.
+  inbox =  await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ from : accounts[0], gas: "1000000" })
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-
+    console.log(inbox);
   })
 })
