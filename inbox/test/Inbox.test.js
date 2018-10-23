16b3cdd0e8731467d3114a52d2c380605cea1e8b
Jason Kiraly <jaykiraly@gmail.com>
2018-10-23 12:44:59 -0700

    added web3 instance.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,3 +2,6 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
+// Provider might change depending what network we point to on the blockchain.
+const web3 = new Web3(ganache.provider());
+
