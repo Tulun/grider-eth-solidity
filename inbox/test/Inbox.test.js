18cda2b305b166ce73ab62193791ec1d701ecada
Jason Kiraly <jaykiraly@gmail.com>
2018-10-23 13:00:05 -0700

    accounts created. woo

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -5,3 +5,16 @@ const Web3 = require('web3');
 // Provider might change depending what network we point to on the blockchain.
 const web3 = new Web3(ganache.provider());
 
+beforeEach( () => {
+  // Get a list of all accounts
+  const accounts = web3.eth.getAccounts().then( fetchedAccounts => {
+    console.log(fetchedAccounts);
+  })
+  // Use one of the accounts to deploy.
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+
+  })
+})
