4c2a93dcdadf2fc02ee7568726261cae1d9240ed
Jason Kiraly <jaykiraly@gmail.com>
2018-10-23 10:47:17 -0700

    Export our contract.

--- inbox/compile.js
+++ inbox/compile.js
@@ -5,5 +5,4 @@ const solc = require('solc')
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source  = fs.readFileSync(inboxPath, "utf8");
 
-const compiledInbox = solc.compile(source, 1);
-console.log(compiledInbox);
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
