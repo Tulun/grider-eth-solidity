86717a8fdf95be67923546a21b26ca00a18155a8
Jason Kiraly <jaykiraly@gmail.com>
2018-10-23 15:01:23 -0700

    abstracted to constant for message.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -8,12 +8,13 @@ const provider = ganache.provider();
 const web3 = new Web3(provider);
  
 let accounts, inbox;
+const INITIAL_MESSAGE = "Hi there!"
  beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
   // Use one of the accounts to deploy.
   inbox =  await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
     .send({ from : accounts[0], gas: "1000000" })
 
   inbox.setProvider(provider);
@@ -26,7 +27,7 @@ describe('Inbox', () => {
 
   it('has a default message', async () => {
     const message = await inbox.methods.message().call();
-    assert.equal(message, 'Hi there!');
+    assert.equal(message, INITIAL_MESSAGE);
   });
 
 })
