ce0d3a8760cf2e29bf35082babeddf93b11278af
Jason Kiraly <jaykiraly@gmail.com>
2018-10-24 17:33:39 -0700

    fixed lint issues.

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -1,27 +1,29 @@
 pragma solidity ^0.4.24;
 
+
 contract Lottery {
-    address public manager;
-    address[] public players;
-    
-    constructor() public {
-        manager = msg.sender;
-    }
-    
-    function enter() public payable {
-        require(msg.value > .01 ether);
-        players.push(msg.sender);
-    }
-    
-    function random() private view returns (uint) {
-       return uint(keccak256(
-           abi.encodePacked(block.difficulty, now, players)
-        ));
-    }
-    
-    function pickWinner() public payable {
-        uint index = random() % players.length;
-        players[index].transfer(address(this).balance);
-        players = new address[](0);
-    }
+  address public manager;
+  address[] public players;
+  
+  constructor() public {
+    manager = msg.sender;
+  }
+  
+  function enter() public payable {
+    require(msg.value > .01 ether);
+    players.push(msg.sender);
+  }
+
+  function pickWinner() public payable {
+    require(msg.sender == manager);
+    uint index = random() % players.length;
+    players[index].transfer(address(this).balance);
+    players = new address[](0);
+  }
+  
+  function random() private view returns (uint) {
+    return uint(keccak256(abi.encodePacked(block.difficulty, now, players)));
+  }
+  
+
 }
