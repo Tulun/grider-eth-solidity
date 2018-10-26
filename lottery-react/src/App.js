50c8da0e76c46766aaabccf918c48901158d4dcf
Jason Kiraly <jaykiraly@gmail.com>
2018-10-26 14:33:43 -0700

    Added players and balance to state.

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
   state = {
-    manager: ""
+    manager: "",
+    players: [],
+    balance: ""
   }
 
   async componentDidMount() {
     // Leave out from because of MetaMask injection.
     const manager = await lottery.methods.manager().call();
-    this.setState({ manager });
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
   }
 
   render() {
